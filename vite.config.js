6355a9c85dd38efe70b091785919b029141796a2
joker <jocker.ss.cat@gmail.com>
2024-04-23 20:32:35 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: import.meta.env.NODE_ENV === 'production' ? '/nodejs_weak04_midExam/' : '/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
