3df7229c25f642cda024e6c41084511209f29eaf
joker <jocker.ss.cat@gmail.com>
2024-04-26 16:09:11 +0800

    移除: footerComponents.vue & axio: footerComponents.vue &  default setting

--- src/main.js
+++ src/main.js
@@ -14,17 +14,10 @@ import VueAxios from 'vue-axios'
 
 // 註冊全域組件
 import NavbarComponent from '@/components/NavbarComponent.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
 
 const app = createApp(App)
 
-// const api = import.meta.env.VITE_API
-// app.config.globalProperties.$axios = axios.create({
-//   baseURL: api
-// })
-
 app.component('NavbarComponent', NavbarComponent)
-app.component('FooterComponent', FooterComponent)
 
 app.use(VueAxios, axios)
 app.use(createPinia())
