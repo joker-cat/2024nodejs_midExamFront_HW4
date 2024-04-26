ec79b2a8eb579484e8d1cef41384b5a341f4f3e4
joker <jocker.ss.cat@gmail.com>
2024-04-26 15:50:05 +0800

    註解: 本地測試上傳imgur 127.0.0.1 IP

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  // localhost 無法上傳圖片
-  server: {
-    host: '127.0.0.1' // 設定伺服器的主機位址，可以是特定的 IP 位址或 'localhost'/'127.0.0.1'
   }
+  // localhost 無法上傳圖片
+  // server: {
+  //   host: '127.0.0.1' // 設定伺服器的主機位址，可以是特定的 IP 位址或 'localhost'/'127.0.0.1'
+  // }
 })
